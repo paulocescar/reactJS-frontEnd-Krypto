ac1c72006f8b15045ab21df38529cf4fbd5bb941
paulocescar <36766785+paulocescar@users.noreply.github.com>
2022-01-24 01:20:29 -0300

    iniciando

---
+++ krypto/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import { H1MenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+
+const Navbar = () => {
+    return (
+        <h1>Navbar</h1>
+    )
+}
+
+export default Navbar;
